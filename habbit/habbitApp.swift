0b84bd94ea19d1305f2e400cad6abc6edb361944
Ruitao Chen <ruitaocchen@gmail.com>
2026-02-16 09:02:02 -0500

    Initial Commit

---
+++ habbit/habbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  habbitApp.swift
+//  habbit
+//
+//  Created by Ruitao Chen on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct habbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
